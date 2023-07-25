791638e22e119913a632b84e9ef2cc130e919017
enoch <enokkang@gmail.com>
2023-07-25 12:38:51 -0500

    adds resources method instead of get methods in route file

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "articles#index"
 
-  get "/articles", to: "articles#index"
-  get "/articles/:id", to: "articles#show"
+  resources :articles
+
+  #get "/articles", to: "articles#index"
+  #get "/articles/:id", to: "articles#show"
 end
